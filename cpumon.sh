e988d2416fdeb6c65e72a1b929318128d9d73222
bclittleua <32556428+bclittleua@users.noreply.github.com>
2024-10-14 10:43:39 -0700

    Create cpumon.sh

---
+++ cpumon.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+(
+cpuTemp0=$(cat /sys/class/thermal/thermal_zone0/temp)
+cpuTemp1=$(($cpuTemp0 / 1000))
+cpuTemp2=$(($cpuTemp0 / 100))
+cpuTempM=$(($cpuTemp2 % $cpuTemp1))
+echo $(date '+%D,%H:%M:%S,')$cpuTemp1.$cpuTempM
+) &>> /home/pi/bin/log-cpumon
