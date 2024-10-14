66aa2f7aa1b9526c6380160e86301948aa5f11f8
bclittleua <32556428+bclittleua@users.noreply.github.com>
2024-10-14 11:37:36 -0700

    Update hook.py

--- hook.py
+++ hook.py
@@ -8,7 +8,7 @@ You will need to create your own webhook to replace the URL below.
 6. click on new webhook and give it a new name, assign a default channel, and finally:
 7. click Copy Webhook URL button
 
-obvioulsy, requires the discord lib for python. install with pip.
+obvioulsy, requires the discord lib for python. install with ''pip install discord''.
 '''
 import sys
 from discord import Webhook, RequestsWebhookAdapter
